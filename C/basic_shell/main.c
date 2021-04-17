3fed74055b5699fd1a52ab408ae5dab5aaccfa87
seb-romero <82671838+seb-romero@users.noreply.github.com>
2021-04-16 22:15:24 -0500

    Update main.c
    ciclo while para uso repetido del minishell, todavia falta arreglar algunas cosas

--- C/basic_shell/main.c
+++ C/basic_shell/main.c
@@ -2,16 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <assert.h>
 #include "utils.h"
 
 int main(void)
 {
     char* argv[256];
+    int status;
+    pid_t pid;
 
-    printf(":| &> ");
-    read_command(argv);
-    
-    execvp(argv[0], argv);
+    while(1){
+        printf(":| &> ");
+        read_command(argv);
+        pid = fork();
+        assert (pid>=0);
+
+
+        if(pid==0){ //HIJO
+            execvp(argv[0], argv);
+        }
+        else{ //PADRE
+            /*wait(&estado);
+            if(WIFEXITED(estado)==0){
+                printf(":^]\n");
+            } else{
+                printf(":^[\n");
+            }*/
+                read_command(argv);
+                fork();
+                wait(&status);
+            
+        }
+    }
     
     return 0;
 }
